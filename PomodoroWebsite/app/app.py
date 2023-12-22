14dac5e4880acf36020c19a8e6a7cde533957067
Anton Joshua Dominguez <143872285+antonjoshuadominguez@users.noreply.github.com>
2023-12-22 23:14:15 +0800

    update websites
    -fixed the websites
    -made routes to websites work

--- PomodoroWebsite/app/app.py
+++ PomodoroWebsite/app/app.py
@@ -3,7 +3,7 @@ from .models import db
 from .routes import routes_bp
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, template_folder='templates')
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pomodoro.db'
     
     db.init_app(app)
